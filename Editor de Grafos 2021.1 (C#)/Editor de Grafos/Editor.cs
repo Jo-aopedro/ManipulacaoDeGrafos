6b71cb48cbded5099774c44f3c7a23f245945045
João Pedro <79295410+Jo-aopedro@users.noreply.github.com>
2021-09-16 17:09:16 -0300

    Add files via upload

---
+++ Editor de Grafos 2021.1 (C#)/Editor de Grafos/Editor.cs
@@ -0,0 +1,197 @@
+﻿using Microsoft.VisualBasic;
+using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Drawing.Drawing2D;
+using System.IO;
+using System.Reflection;
+using System.Windows.Forms;
+
+namespace Editor_de_Grafos
+{
+    public partial class Editor : Form
+    {
+        public Editor()
+        {
+            InitializeComponent();            
+        }
+
+        #region Botoes de Algoritmo do Menu
+        private void BtParesOrd_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show(g.paresOrdenados(), "Os pares ordenados do grafo são: ", MessageBoxButtons.OK, MessageBoxIcon.Information);
+        }
+
+        private void BtGrafoEuleriano_Click(object sender, EventArgs e)
+        {
+            if(g.isEuleriano())
+                MessageBox.Show("O grafo e Euleriano!", "Mensagem", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            else
+                MessageBox.Show("O grafo não e Euleriano!", "Mensagem", MessageBoxButtons.OK, MessageBoxIcon.Information);
+        }
+
+        private void BtGrafoUnicursal_Click(object sender, EventArgs e)
+        {
+            if (g.isUnicursal())
+                MessageBox.Show("O grafo e Unicursal!", "Mensagem", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            else
+                MessageBox.Show("O grafo não e Unicursal!", "Mensagem", MessageBoxButtons.OK, MessageBoxIcon.Information);
+        }
+
+        private void buscaEmProfundidadeToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            if(g.getVerticeMarcado() == null)
+            {
+                g.chamadaProfundidade(0);
+            }
+            else
+                g.chamadaProfundidade(g.getVerticeMarcado().getNum());
+            
+        }
+
+        private void buscaEmLarguraToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            if (g.getVerticeMarcado() == null)
+            {
+                g.largura(0);
+            }
+            else
+                g.largura(g.getVerticeMarcado().getNum());
+            
+
+        }
+
+        private void éArvoreToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            if (g.isArvore())
+                MessageBox.Show("O grafo é Uma Árvore!", "Mensagem", MessageBoxButtons.OK);
+            else
+                MessageBox.Show("O grafo não é Uma Árvore!", "Mensagem", MessageBoxButtons.OK);
+
+        }
+
+        private void caminhoMínimoToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            if (g.getVerticeMarcado() == null)
+            {
+                MessageBox.Show("Selecione o vértice de origem antes de iniciar.", "Atenção", MessageBoxButtons.OK);
+            }
+            else
+            {
+                int v = Convert.ToInt32(Interaction.InputBox("Digite o número do vertice de destino (ex: vértice v1 = 1): "));
+                g.caminhoMinimo(g.getVerticeMarcado().getNum(), v - 1);
+
+                MessageBox.Show(g.getCustoCaminho(), "O custo do caminho é:", MessageBoxButtons.OK);
+            }
+
+        }
+
+        private void arvoreGeradoraMinimaToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            if (g.getVerticeMarcado() == null)
+            {
+                g.AGM(0);
+                MessageBox.Show(g.getCustoCaminho(), "O custo da AGM é:", MessageBoxButtons.OK);
+            }
+            else
+            {
+                g.AGM(g.getVerticeMarcado().getNum());
+                MessageBox.Show(g.getCustoCaminho(), "O custo da AGM é:", MessageBoxButtons.OK);
+            }
+        }
+
+        private void númeroCromáticoToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            g.numeroCromatico();
+            MessageBox.Show(Convert.ToString(g.getNumeroCromático()), "O nº Cromático é:", MessageBoxButtons.OK);
+        }
+
+        #endregion --------------------------------------------------------------------------------------------------
+
+        #region botoes restantes do menu
+
+        private void BtNovo_Click(object sender, EventArgs e)
+        {
+            g.limpar();
+        }
+
+        private void BtAbrir_Click(object sender, EventArgs e)
+        {
+            if(OPFile.ShowDialog() == DialogResult.OK)
+            {
+                g.abrirArquivo(OPFile.FileName);
+                g.Refresh();
+            }
+        }
+
+        private void BtSalvar_Click(object sender, EventArgs e)
+        {
+            if(SVFile.ShowDialog() == DialogResult.OK)
+            {
+                g.SalvarArquivo(SVFile.FileName);
+            }
+        }
+
+        private void BtSair_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void BtPeso_Click(object sender, EventArgs e)
+        {
+            if(BtPeso.Checked)
+            {
+                BtPeso.Checked = false;
+                g.setExibirPesos(false);
+
+            }
+            else
+            {
+                BtPeso.Checked = true;
+                g.setExibirPesos(true);
+            }
+            g.Refresh();
+        }
+
+        private void BtPesoAleatorio_Click(object sender, EventArgs e)
+        {
+            if(BtPesoAleatorio.Checked)
+            {
+                BtPesoAleatorio.Checked = false;
+                g.setPesosAleatorios(false);
+            }
+            else
+            {
+                BtPesoAleatorio.Checked = true;
+                g.setPesosAleatorios(true);
+            }
+        }
+
+        private void BtSobre_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Editor de Grafos - 2021/1\n\nDesenvolvido por:\nNatan Macedo de Magalhaes\nVirgilio Borges de Oliveira\nJoão Pedro Martins de Moura \n\nAlgoritmos e Estruturas de Dados II\nFaculdade COTEMIG\nSomente para fins didáticos.", "Sobre o Editor de Grafos...", MessageBoxButtons.OK, MessageBoxIcon.Information);
+        }
+
+        #endregion --------------------------------------------------------------------------------------------------
+
+        private void completarGrafoToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            g.completarGrafo();
+        }
+
+        private void ferramentasToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void algoritmosToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void limparTelaToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            g.limparCores();
+        }
+    }
+}
---
+++ Editor de Grafos 2021.1 (C#)/Editor de Grafos/Grafo.cs
@@ -0,0 +1,339 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Drawing;
+
+namespace Editor_de_Grafos
+{
+    public class Grafo : GrafoBase, iGrafo
+    {
+        private bool[] visitado;
+        private string Custo;
+        private int numeroCromático;
+        Random aleatorio = new Random();
+
+        public void limparCores() // Função pra limpar as cores do grafo depois de um metodo
+        {
+
+            for (int i = 0; i < getN(); i++) 
+            {
+                getVertice(i).setCor(Color.Chocolate);
+                for (int j = 0; j < getN(); j++)
+                {
+                    if(getAresta(i,j)!=null)
+                        getAresta(i, j).setCor(Color.Black);
+                }
+            }
+        }
+        public void AGM(int v)
+        {
+            limparCores();
+
+            int[] antecessor = new int[getN()];// usado na hora de setar as arestas
+            int[] estimativa = new int[getN()];// usado pra pegar sempre os menores caminhos
+            bool[] fechados = new bool[getN()];// Tipo um "Visitados", só que tirei da ideia do Camino mínimo
+            Custo = "";
+
+            int custoAuxiliar = 0, min = 1000000;// O custo é uma var cumulativa pra sair no retorno no final com o valor 
+            fechados[v] = true;
+            for (int j=0; j< getN()-1;j++)
+            {
+                for (int i = 0; i < getN(); i++)
+                {
+                    if (getAresta(v, i) != null && !fechados[i])
+                    {
+                        if (estimativa[i] != 0 && estimativa[i] > getAresta(v, i).getPeso()) // serve para V ja visitados mas que possam ter estm. maior que a atual
+                        {
+                            estimativa[i] = getAresta(v, i).getPeso();
+                            antecessor[i] = v;
+                        }
+                        else if (estimativa[i] == 0)
+                        {
+                            estimativa[i] = getAresta(v,i).getPeso();
+                            antecessor[i] = v;
+                        }
+                    }
+                }
+                for (int i = 0; i < getN(); i++) // escolhe a aresta c/menor custo
+                {
+                    if (estimativa[i] < min && !fechados[i] && estimativa[i] != 0)
+                    {
+                        min = estimativa[i];
+                        v = i;
+                    }
+                }
+                custoAuxiliar += getAresta(antecessor[v], v).getPeso();
+                getAresta(antecessor[v], v).setCor(System.Drawing.Color.YellowGreen);
+                fechados[v] = true;
+                min = 1000000;//reseta o min
+            }
+            Custo += custoAuxiliar;
+        }
+        public void caminhoMinimo(int c, int f)
+        {
+            limparCores();
+            // Tres vetores do Djkstra
+            int[] antecessor = new int[getN()];
+            int[] estimativa = new int[getN()];
+            bool[] fechados = new bool[getN()];
+            Custo = "";
+
+            int min = 1000000, inicial = c, destino = f;
+            fechados[c] = true;
+            while (c != f) { 
+                for (int v = 0; v < getN(); v++)
+                {
+                    if (getAresta(c, v) != null && !fechados[v])
+                    {
+                        if (estimativa[v] != 0 && estimativa[v] > (getAresta(c, v).getPeso() + estimativa[c])) // serve para V ja visitados mas que possam ter estm. maior que a atual
+                        {
+                            estimativa[v] = (getAresta(c, v).getPeso() + estimativa[c]);
+                            antecessor[v] = c;
+                        }
+                        else if(estimativa[v]==0)
+                        {
+                            estimativa[v] = (getAresta(c, v).getPeso() + estimativa[c]);
+                            antecessor[v] = c;
+                        }
+                    }
+                }
+                for (int i = 0; i < getN(); i++)
+                {
+                    if (estimativa[i] < min && !fechados[i] && estimativa[i]!=0)
+                    {
+                        min = estimativa[i];
+                        c = i;
+                    }
+                }
+                fechados[c] = true;
+                min = 1000000;
+            }
+            while (f != inicial)
+            {
+                getAresta(antecessor[f], f).setCor(System.Drawing.Color.YellowGreen);
+                f = antecessor[f];
+            }
+            Custo += estimativa[destino];
+        }
+        public string getCustoCaminho()
+        {
+            return Custo;
+        }
+
+        public void completarGrafo() 
+        {
+            for (int i = 1; i <= getN(); i++)
+            {
+                for (int j = 0; j < i; j++)
+                {
+                    if (getAresta(i,j) == null)
+                    {
+                        if (getPesosAleatorios()) // Se o pesos aleatórios estiver marcado ele completa ja com eles
+                        {
+                            setAresta(i, j, aleatorio.Next(1, 100));
+                        }
+                        else
+                            setAresta(i, j, 1);
+                    }
+                }
+            }
+
+            limparCores();
+        }
+
+        public bool isEuleriano()
+        {
+            int i;
+            for (i = 0; i < getN(); i++)
+            {
+                if (grau(i) % 2 != 0)
+                    return false;
+
+            }
+            if (getN() != 0)
+                return true;
+            else
+                return false;
+        }
+
+        public bool isUnicursal()
+        {
+            int i;
+            int impar = 0;
+            for (i = 0; i < getN(); i++)
+            {
+                if (grau(i) % 2 != 0)
+                    impar++;
+
+            }
+            if (getN() != 0 && impar==2)
+                return true;
+            else
+                return false;
+        }
+
+        public void largura(int v)
+        {
+
+            limparCores();
+
+            visitado = new bool[getN()];
+            Fila f = new Fila(matAdj.GetLength(0));
+
+            f.enfileirar(v);
+            visitado[v] = true;
+            while (!f.vazia())
+            {
+                v = f.desenfileirar();
+                for (int i = 0; i < getN(); i++)
+                {
+                    if(getAresta(v, i) != null && !visitado[i])
+                    {
+                        getAresta(v, i).setCor(System.Drawing.Color.YellowGreen);
+                        visitado[i] = true;
+                        f.enfileirar(i);
+                    }
+                }
+            }
+        }
+
+        public void numeroCromatico()
+        {
+            limparCores();
+
+            visitado = new bool[getN()];
+            Fila f = new Fila(getN());
+            Color[] cor = { Color.Purple, Color.Green, Color.Blue, Color.Yellow, Color.DarkRed, Color.HotPink, Color.ForestGreen, Color.RosyBrown, Color.Silver, Color.Black };
+            int maiorGrau = -1, v = 0,c=1;
+            int[] corDoVertice = new int[getN()]; // auxiliar pra indicar o indice do vetor cor
+            bool continua = true;
+            numeroCromático = 0; 
+
+            for (int i = 0; i < getN(); i++)//pega pelo v de maior grau
+            {
+               if(grau(i) > maiorGrau)
+                {
+                    maiorGrau = grau(i);
+                    v = i;
+                }
+            }
+
+            f.enfileirar(v);
+            visitado[v] = true;
+            corDoVertice[v] = 1;
+            getVertice(v).setCor(cor[0]);
+            while (!f.vazia())
+            {
+                v = f.desenfileirar();
+                for (int i = 0; i < getN(); i++)
+                {
+                    if (getAresta(v, i) != null && !visitado[i])
+                    {
+                        while (continua)
+                        {
+                            for(int j = 0; j < getN(); j++)
+                            {
+                                if(getAresta(i,j)!= null && corDoVertice[j]== c)
+                                {
+                                    j = getN();
+                                    c++;
+                                }
+                                else if (j == getN() - 1)
+                                {
+                                    corDoVertice[i] = c;
+                                    getVertice(i).setCor(cor[(c - 1)]);
+                                    continua = false;
+                                }
+                            }
+                        }
+                        visitado[i] = true;
+                        c = 1;
+                        continua = true;
+                        f.enfileirar(i);
+                    }
+                }
+            }
+            for (int i = 0; i < getN(); i++)
+            {
+                if (corDoVertice[i] > c )
+                {
+                    c = corDoVertice[i+1];
+                }
+            }
+            numeroCromático = c;
+
+        }
+        public int getNumeroCromático()
+        {
+            return numeroCromático;
+        }
+
+        public String paresOrdenados()
+        {
+            string pares = "{ ";
+            for (int i = 0; i <=getN(); i++)
+            {
+                for (int j = 0; j < getN(); j++)
+                {
+                    if (getAresta(i,j) != null)
+                    {
+                        pares += "(" + i + "," + j + ")" + ",";
+                    }
+
+                }
+            }
+            pares = pares.Substring(0,pares.Length-1) + "}";
+            return pares;
+        }
+        public void profundidade(int v)
+        {
+
+            visitado[v] = true;
+            for (int i = 0; i < getN(); i++)
+            {
+                if(getAresta(v, i) != null && ! visitado[i])
+                {
+                    getAresta(v, i).setCor(System.Drawing.Color.YellowGreen); // tem o System.Drawning pq não vi que ainda não tinha percebido a falta da Library
+                    profundidade(i);
+                }
+            }
+
+        }
+        public void chamadaProfundidade(int v) // Precisou de método de chamada que o profundidade é recursivo e instanciar vetor direto nele ia dar problema
+        {
+            limparCores();
+            visitado = new bool[getN()];
+            profundidade(v);
+        }
+
+        public bool isArvore() // Usa o largura.
+        {
+            int v = 0;
+            visitado = new bool[getN()];
+            Fila f = new Fila(getN());
+            int[] antecessor = new int[getN()];
+
+            f.enfileirar(v);
+            visitado[v] = true;
+            antecessor[v] = v;
+            while (!f.vazia())
+            {
+                v = f.desenfileirar();
+                for (int i = 0; i < getN(); i++)
+                {
+                    if (getAresta(v,i) != null && !visitado[i])
+                    {
+                        visitado[i] = true;
+                        f.enfileirar(i);
+                        antecessor[i] = v;
+                    }
+                    else if (getAresta(v, i) != null && visitado[i] && antecessor[v] != i) // se o v visita um outro v ja visitado que não é seu pai, não é arvore
+                    {
+                        return false;
+                    }
+                }
+            }
+            return true;
+        }
+    }
+}
